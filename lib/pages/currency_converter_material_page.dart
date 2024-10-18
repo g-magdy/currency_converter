54fadd4555c27a4867e723e241fbfe40f2255b66
g-magdy <george.hanna03@eng-st.cu.edu.eg>
2024-10-18 18:31:50 +0300

    add button and its styling

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
@@ -12,22 +13,23 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
       ),
     );
 
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               'Hello',
               style: TextStyle(
                   fontSize: 45,
                   fontWeight: FontWeight.bold,
                   color: Colors.white),
             ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
+              child: const TextField(
                 cursorColor: Colors.white,
                 style: TextStyle(color: Colors.black),
                 decoration: InputDecoration(
@@ -41,6 +43,23 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
                   enabledBorder: border,
                 ),
                 keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print("Button Clicked");
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 15,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text("Convert"),
               ),
             ),
           ],
